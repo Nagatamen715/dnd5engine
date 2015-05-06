ba88703489212be077320631b2acd694a84d0e9c
Benjamin Hager <benjaminhager@gmail.com>
2015-05-05 21:42:56 -0700

    Added characterSheet
    Started writing characterSheet struct. I'll add more after I get it
    setup for it's current iteration.

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	fmt.Println("DnD 5E Engine Main Menu")
-
+	
 	/*	Block of code to generate ability modifiers
 		Uses the equation abilityModifier = (baseStat-10)/2
-	
+
 		Sample Values:
 		Ability		Modifier
 		1			-5
@@ -23,19 +23,53 @@ func main() {
 		12-13		+1
 		14-15		+2
 		etc...
-	
-		transfer these functions to charcterSheet struct when ready 
+
+		transfer these functions to charcterSheet struct when ready
 	*/
 	var baseStat, abilityModifier, mathCheck int
-	for baseStat = 1; baseStat < 20; baseStat++ {
-		mathCheck = baseStat - 10
 
-		// Code rounds down, so this corrects mistakes with negative numbers
-		if mathCheck < 0 {
-			mathCheck--
-		}
+	mathCheck = baseStat - 10
 
-		abilityModifier = mathCheck / 2
-		fmt.Println("baseStat: ", baseStat, "mathCheck: ", mathCheck, "abilityModifier: ", abilityModifier)
+	// Code rounds down, so this corrects mistakes with negative numbers
+	if mathCheck < 0 {
+		mathCheck--
 	}
+
+	abilityModifier = mathCheck / 2
+	fmt.Println("baseStat: ", baseStat, "mathCheck: ", mathCheck, "abilityModifier: ", abilityModifier)
+	
+	baseStat = 19
+	fmt.Println(baseStat % 18)
+}
+
+type characterSheet struct {
+	
+	// Base Ability Scores
+	var strength int 
+	var dexterity int 
+	var constitution int
+	var intelligence int  
+	var wisdom int 
+	var charisma int 
+	
+	// Hit Point Variables
+	var hitPointMax int
+	var hitPointCurrent int 
+	var hitDiceType int
+	var hitDiceMax int 
+	var hitDiceCurrent int
+	//var temporaryHitPoints int (disabled until I decide how to tackle this)
+	
+	// Defensive Scores
+	var armorClass int
+	var strengthSavingThrow int
+	var dexteritySavingThrow int
+	var constitutionSavingThrow int
+	var intelligenceSavingThrow int
+	var wisdomSavingThrow int
+	var charismaSavingThrow int
+	var deathSavingThrowSucess int
+	var deathSavingThrowFailure int
+	
+
 }
